d6a84d32cef2a77df5ae008c7448367fb436673c
anurajbh <anurajbh@users.noreply.github.com>
2018-10-15 08:09:17 -0400

    Rabbit
    Added walking rabbit.

---
+++ Assets/WR_wHiteRabbiT-sTudio/GraphLibrary/Model/Merged/Characters/Cartoon/Rabbit/rabbitWalk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class rabbitWalk : MonoBehaviour
+{
+
+    public Transform destination;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(destination.position);
+    }
+
+}
+
